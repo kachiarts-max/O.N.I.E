e13fdd8bfb6433c69f04d131c7a6a275467f03f9
kachiarts-max <onyekachichigozie581@gmail.com>
2026-08-17 09:38:16 +0100

    Add Compose microphone icon dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,7 @@ dependencies {
     implementation("androidx.compose.foundation:foundation")
     implementation("androidx.compose.animation:animation")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.compose.material:material-icons-core")
 
     debugImplementation("androidx.compose.ui:ui-tooling")
