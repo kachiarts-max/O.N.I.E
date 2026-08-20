20ee9ac4bf31ab51f0ee4501f95b889826422c76
kachiarts-max <onyekachichigozie581@gmail.com>
2026-08-20 06:29:09 +0100

    Clean up ONIE session service

--- app/src/main/java/com/onie/assistant/voice/ONIESessionService.kt
+++ app/src/main/java/com/onie/assistant/voice/ONIESessionService.kt
@@ -1,11 +1,12 @@
 package com.onie.assistant.voice
 
+import android.os.Bundle
 import android.service.voice.VoiceInteractionSession
 import android.service.voice.VoiceInteractionSessionService
 
 class ONIESessionService : VoiceInteractionSessionService() {
 
-    override fun onNewSession(args: android.os.Bundle?): VoiceInteractionSession {
+    override fun onNewSession(args: Bundle?): VoiceInteractionSession {
         return ONIESession(this)
     }
 }
